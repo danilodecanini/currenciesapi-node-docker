611711df9a26b75b134fd02abbbde6fc53f6140d
Danilo de Canini <danilodecanini@gmail.com>
2019-08-13 14:05:35 -0300

    alguns ajustes de conexão com o banco

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
-const express = require('express');
-const routes = require('./routes');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 /*
-* Setting up the database (mongodb) 
-*/ 
+ * Setting up the database (mongodb)
+ */
 
-mongoose.connect('mongodb://db:27017/curriencies', {
-    useNewUrlParser: true
-});
+if (process.env.ENVIRONMENT == "production") {
+    console.info("Mongoose: Mongo starting on production mode");
 
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_COLLETION}?retryWrites=true&w=majority`, {
+        useNewUrlParser: true
+    });
+} else {
+    console.info("Mongoose: Mongo starting on development mode");
+
+    mongoose.connect("mongodb://db:27017/curriencies", {
+        useNewUrlParser: true
+    });
+}
 
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Mongoose: Connection Error: '));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "Mongoose: Connection Error: "));
+db.once("open", function() {
   console.info("Mongoose: Connected!");
 });
 
-/* 
-* Starting the server
-*/
+/*
+ * Starting the server
+ */
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Express: Server activated with port 3333'));
+app.listen(3333, () => console.log("Express: Server activated with port 3333"));
